f285ed7b22cd0c85aea44f096e349a5c32e6a4ce
yxj <564197835@qq.com>
2023-06-02 04:49:17 +0800

    add all values

--- ExARFaceAnchor/FaceModel.swift
+++ ExARFaceAnchor/FaceModel.swift
@@ -6,14 +6,21 @@ struct FaceModel {
     var rightEyeTransform = simd_float4x4()
     var lookAtPoint = simd_float3()
 
+    var geometry: ARFaceGeometry?
+    var blendShapes: [ARFaceAnchor.BlendShapeLocation: NSNumber] = [:]
+
     var console = ""
 
     // https://developer.apple.com/documentation/arkit/arfaceanchor
     mutating func update(faceAnchor: ARFaceAnchor) {
         transform = faceAnchor.transform
+        geometry = faceAnchor.geometry
+        blendShapes = faceAnchor.blendShapes
+
         leftEyeTransform = faceAnchor.leftEyeTransform
         rightEyeTransform = faceAnchor.rightEyeTransform
         lookAtPoint = faceAnchor.lookAtPoint
+
         console = "\(faceAnchor.transform)"
     }
 }
